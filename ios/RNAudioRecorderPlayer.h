0e37150361a302e95a3d10ed989c6df8c1168d85
dmitry <avramenko150@gmail.com>
2024-04-18 17:40:18 +0300

    WIP ios turbomodule support

--- ios/RNAudioRecorderPlayer.h
+++ ios/RNAudioRecorderPlayer.h
@@ -1,10 +1,16 @@
+#import <React/RCTEventEmitter.h>
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <RNAudioRecorderPlayerSpec/RNAudioRecorderPlayerSpec.h>
+
+@interface RNAudioRecorderPlayer : RCTEventEmitter <NativeCalculatorSpec, AVAudioPlayerDelegate>
+#else
 @interface RNAudioRecorderPlayer : RCTEventEmitter <RCTBridgeModule, AVAudioPlayerDelegate>
+#endif
 - (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag;
 - (void)audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error;
 - (void)updateRecorderProgress:(NSTimer*) timer;
+
 @end
