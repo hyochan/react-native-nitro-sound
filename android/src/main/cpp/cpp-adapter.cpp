0020b0c4f15caa209881a0b87a9b1706bae5ce7e
Radek Czemerys <7029942+radko93@users.noreply.github.com>
2026-03-12 01:46:30 +0800

    chore: upgrade Nitro to 0.35.0
    Co-Authored-By: Ritesh Shukla <riteshshukla2381@gmail.com>

--- android/src/main/cpp/cpp-adapter.cpp
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,9 @@
 #include <jni.h>
+#include <fbjni/fbjni.h>
 #include "NitroSoundOnLoad.hpp"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::sound::initialize(vm);
+  return facebook::jni::initialize(vm, [=] {
+    margelo::nitro::sound::registerAllNatives();
+  });
 }
